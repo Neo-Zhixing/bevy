f29cd14db6402e498368aa8998fad9e5a1dec1ad
Zhixing Zhang <me@neoto.xin>
2024-01-07 01:52:04 -0800

    Remove redundant method

--- crates/bevy_ecs/src/schedule/schedule.rs
+++ crates/bevy_ecs/src/schedule/schedule.rs
@@ -465,11 +465,6 @@ impl SystemNode {
         }
     }
 
-    pub fn initialize(&mut self, world: &mut World) {
-        let system = self.get_mut().unwrap();
-        system.initialize(world);
-    }
-
     pub fn get(&self) -> Option<&BoxedSystem> {
         self.inner.as_ref()
     }
